427db8292e8a9ea1825d5ef650a23eaddb424998
uzair-7886 <jerry.khan7886@gmail.com>
2023-07-19 14:19:19 +0500

    test clerk

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import {
   UserButton,
   useUser,
   RedirectToSignIn,
+  SignIn,
+  SignUp
 } from "@clerk/clerk-react";
 // import dotenv from 'dotenv';
 import Todo from './components/Todo';
 import './App.css'
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
 
 
 const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
@@ -18,18 +21,47 @@ if (!clerkPubKey) {
   throw new Error("Missing Publishable Key")
 }
 
-
-
-function App() {
-  return (
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <SignedIn>
+const ClerkProviderWithRoutes=()=>{
+  const navigate = useNavigate();
+  return(
+<ClerkProvider
+     publishableKey={clerkPubKey}
+     navigate={(to) => navigate(to)}
+     >
+      <Routes>
+      <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+         <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+        path='/'
+        element={
+          <>
+          <SignedIn>
         <Todo/>
       </SignedIn>
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
+          </>
+        }
+        />
+      </Routes>
     </ClerkProvider>
+  )
+}
+
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+    <ClerkProviderWithRoutes/>
+    </BrowserRouter>
   );
 }
 
